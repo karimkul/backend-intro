b8b2503cc34aeb9dd6d40dbca2b5f095f23db968
Jami Karimkulov <jami.karimkul@gmail.com>
2024-08-16 12:39:49 -0400

    Build a very simple API

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -35,12 +35,19 @@ console.log("Read file1");
 
 /////////////////////////////////////////////////
 // SERVER
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, `utf-8`);
+const dataObj = JSON.parse(data);
+
 const server = http.createServer((req, res) => {
     const pathName = req.url;
     if (pathName === "/" || pathName === "/overview") {
         res.end("This is OVERVIEW!");
     } else if (pathName === "/product") {
         res.end("This is PRODUCT!");
+    } else if (pathName === "/api") {
+        res.writeHead(200, { "Content-type": "application/json" });
+        res.end(data);
     } else {
         res.writeHead(404, {
             "Content-type": "text/html",
