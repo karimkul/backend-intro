854fd46f380803297699d2f5508d2d25e85ec930
Jami Karimkulov <jami.karimkul@gmail.com>
2024-11-04 23:49:12 -0500

    Handling get and post requests

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -1,7 +1,8 @@
 const fs = require("fs");
 const express = require("express");
-const { dirname } = require("path");
 const app = express();
+
+app.use(express.json());
 
 // app.get("/", (req, res) => {
 //     res.status(200).json({
@@ -19,10 +20,54 @@ const tours = JSON.parse(
 app.get("/api/v1/tours", (req, res) => {
     res.status(200).json({
         status: "success",
+        results: tours.length,
         data: {
             tours
         }
     });
+});
+
+app.get("/api/v1/tours/:id", (req, res) => {
+    console.log(req.params);
+    const id = req.params.id * 1;
+    const tour = tours.find((el) => el.id === id);
+
+    // if (id > tours.length) {
+    if (!tour) {
+        return res.status(404).json({
+            status: "fail",
+            message: "Invalid Id"
+        });
+    }
+
+    res.status(200).json({
+        status: "success",
+        data: {
+            tour
+        }
+    });
+});
+
+app.post("/api/v1/tours", (req, res) => {
+    // console.log(req.body);
+
+    const newId = tours[tours.length - 1].id + 1;
+    const newTour = Object.assign({ id: newId }, req.body);
+    tours.push(newTour);
+
+    fs.writeFile(
+        `${__dirname}/dev-data/data/tours-simple.json`,
+        JSON.stringify(tours),
+        "utf-8",
+        (err) => {
+            res.status(201).json({
+                status: "success",
+                data: {
+                    tour: newTour
+                }
+            });
+        }
+    );
 });
 
 const port = 3000;
