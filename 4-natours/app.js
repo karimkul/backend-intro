f85201976c47e2ff950659950ac5c1d87875a548
Jami Karimkulov <jami.karimkul@gmail.com>
2024-11-08 11:26:25 -0500

    Create and mounting multiple routes

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -106,6 +106,36 @@ const deleteTour = (req, res) => {
     });
 };
 
+const getAllUsers = (req, res) => {
+    res.status(500).json({
+        status: "error",
+        message: "this route is not yet defined!"
+    });
+};
+const getUser = (req, res) => {
+    res.status(500).json({
+        status: "error",
+        message: "this route is not yet defined!"
+    });
+};
+const createUser = (req, res) => {
+    res.status(500).json({
+        status: "error",
+        message: "this route is not yet defined!"
+    });
+};
+const updateUser = (req, res) => {
+    res.status(500).json({
+        status: "error",
+        message: "this route is not yet defined!"
+    });
+};
+const deleteUser = (req, res) => {
+    res.status(500).json({
+        status: "error",
+        message: "this route is not yet defined!"
+    });
+};
 // app.get("/api/v1/tours", getAllTours);
 // app.get("/api/v1/tours/:id", getTour);
 // app.post("/api/v1/tours", creatTour);
@@ -113,14 +143,34 @@ const deleteTour = (req, res) => {
 // app.delete(`/api/v1/tours/:id`, deleteTour);
 
 // 3) ROUTES
-app.route("/api/v1/tours")
+
+const tourRouter = express.Router();
+const userRouter = express.Router();
+
+tourRouter
+    .route("/")
     .get(getAllTours)
     .post(creatTour);
 
-app.route(`/api/v1/tours/:id`)
-    .get(getAllTours)
+tourRouter
+    .route("/:id")
+    .get(getTour)
     .patch(updateTour)
     .delete(deleteTour);
+
+userRouter
+    .route("/")
+    .get(getAllUsers)
+    .post(createUser);
+
+userRouter
+    .route("/:id")
+    .get(getUser)
+    .patch(updateUser)
+    .delete(deleteUser);
+
+app.use("/api/v1/tours", tourRouter);
+app.use("/api/v1/users", userRouter);
 
 // 4) START SERVER
 const port = 3000;
