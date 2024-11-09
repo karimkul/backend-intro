34a3399026c3b22a6a7eab5ea60d034716d40d97
Jami Karimkulov <jami.karimkul@gmail.com>
2024-11-09 13:25:04 -0500

    Set up eslint + prettier in vs code

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -1,29 +1,30 @@
-const express = require("express");
-const app = express();
-const morgan = require("morgan");
+const express = require('express');
 
-const tourRouter = require("./routes/tourRoutes");
-const userRouter = require("./routes/userRoutes");
+const app = express();
+const morgan = require('morgan');
+
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
 
 // 1) MIDDLEWARES
-if (process.env.NODE_ENV === "development") {
-    app.use(morgan("dev")); ///?????
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev')); ///?????
 }
 
 app.use(express.json()); //??????
-app.use(express.static("./public"));
+app.use(express.static('./public'));
 
 app.use((req, res, next) => {
-    console.log("Hello From the Middleware");
-    next();
+  console.log('Hello From the Middleware');
+  next();
 });
 app.use((req, res, next) => {
-    req.requestTime = new Date().toISOString();
-    next();
+  req.requestTime = new Date().toISOString();
+  next();
 });
 
 // 3) ROUTES
-app.use("/api/v1/tours", tourRouter);
-app.use("/api/v1/users", userRouter);
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
 
 module.exports = app;
--- 4-natours/server.js
+++ 4-natours/server.js
@@ -1,12 +1,12 @@
-const dotenv = require("dotenv");
-dotenv.config({ path: "./config.env" });
+const dotenv = require('dotenv');
+const app = require('./app');
 
-const app = require("./app");
+dotenv.config({ path: './config.env' });
 
 // console.log(app.get("env"));
 // console.log(process.env);
-console.log(process.env.PORT);
+// console.log(process.env.PORT);
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log(`Post is running on port ${port}`);
+  console.log(`Post is running on port ${port}`);
 });
