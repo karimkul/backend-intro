775d3130dd840a907e6334a854d459bfe233d31f
Jami Karimkulov <jami.karimkul@gmail.com>
2024-11-13 14:53:50 -0500

    Update documents using mongoose

--- 4-natours/controllers/tourController.js
+++ 4-natours/controllers/tourController.js
@@ -57,13 +57,25 @@ exports.creatTour = async (req, res) => {
   }
 };
 
-exports.updateTour = (req, res) => {
-  res.status(200).json({
-    status: 'saccess',
-    data: {
-      tour: 'Updated tour here...'
-    }
-  });
+exports.updateTour = async (req, res) => {
+  try {
+    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true
+    });
+
+    res.status(200).json({
+      status: 'saccess',
+      data: {
+        tour
+      }
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: 'Invalid data sent!'
+    });
+  }
 };
 
 exports.deleteTour = (req, res) => {
