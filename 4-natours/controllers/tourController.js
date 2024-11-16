e6055f77f9425477169958f391424ddcee82e1c1
Jami Karimkulov <jami.karimkul@gmail.com>
2024-11-16 11:45:17 -0500

    Make the API better: advanced filtering

--- 4-natours/controllers/tourController.js
+++ 4-natours/controllers/tourController.js
@@ -1,22 +1,29 @@
-// const exspess = require('express');
+const exspess = require('express');
 const Tour = require('../moduls/tourModul');
 
 exports.getAllTours = async (req, res) => {
   try {
     // BUILT QUERY
+    // 1) Filtering
     const queryObj = { ...req.query };
     const excludedFields = ['page', 'sort', 'limit', 'fields'];
     excludedFields.forEach(el => delete queryObj[el]);
 
-    const query = Tour.find(queryObj);
-    // const tours = await Tour.find()
+    // 2) Advanced Filtering
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
+    console.log(JSON.parse(queryStr));
+
+    const query = Tour.find(JSON.parse(queryStr));
+
+    // EXCUTE  QUERY
+    const tours = await query;
+
+    // const query =  Tour.find()
     //   .where('duration')
     //   .equals(5)
     //   .where('difficulty')
     //   .equals('easy');
-
-    // EXCUTE  QUERY
-    const tours = await query;
 
     // SEND RESPOND
     res.status(200).json({
