4601e7924d5880304e7fd8609d0915c5f4cd1390
Jami Karimkulov <jami.karimkul@gmail.com>
2024-11-19 13:50:02 -0500

    Add document middleware

--- 4-natours/moduls/tourModul.js
+++ 4-natours/moduls/tourModul.js
@@ -1,5 +1,6 @@
 // const { describe } = require('eslint/lib/testers/rule-tester');
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -9,6 +10,7 @@ const tourSchema = new mongoose.Schema(
       unique: true,
       trim: true
     },
+    slug: String,
     duration: {
       type: Number,
       required: [true, 'A tour must have a duration']
@@ -64,6 +66,22 @@ const tourSchema = new mongoose.Schema(
 tourSchema.virtual('durationWeeks').get(function() {
   return this.duration / 7;
 });
+
+// DOCUMENT MIDDLEWARE: it runs before .save() and .create() command
+tourSchema.pre('save', function(next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+// tourSchema.pre('save', function(next) {
+//   console.log('We will save the documents...');
+//   next();
+// });
+
+// tourSchema.post('save', function(doc, next) {
+//   console.log(doc);
+//   next();
+// });
 
 const Tour = mongoose.model('Tour', tourSchema);
 
