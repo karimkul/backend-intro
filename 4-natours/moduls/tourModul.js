a58addbc8a09daaecf3a828fa6baaf0a4f3700d0
Jami Karimkulov <jami.karimkul@gmail.com>
2024-11-21 16:20:13 -0500

    Add query middleware

--- 4-natours/moduls/tourModul.js
+++ 4-natours/moduls/tourModul.js
@@ -1,4 +1,5 @@
 // const { describe } = require('eslint/lib/testers/rule-tester');
+const { type } = require('express/lib/response');
 const mongoose = require('mongoose');
 const slugify = require('slugify');
 
@@ -55,7 +56,11 @@ const tourSchema = new mongoose.Schema(
       default: Date.now(),
       select: false
     },
-    startDates: [Date]
+    startDates: [Date],
+    secretTour: {
+      type: Boolean,
+      default: false
+    }
   },
   {
     toJSON: { virtuals: true },
@@ -82,6 +87,21 @@ tourSchema.pre('save', function(next) {
 //   console.log(doc);
 //   next();
 // });
+
+// QUERY MIDDLEWARE
+// tourSchema.pre('find', function(next) {
+tourSchema.pre(/^find/, function(next) {
+  this.find({ secretTour: { $ne: true } });
+  this.start = Date.now();
+  next();
+});
+
+tourSchema.post(/^find/, function(docs, next) {
+  // this.find({ secretTour: { $ne: true } });
+  console.log(`Query took ${Date.now() - this.start} milliseconds`);
+  console.log(docs);
+  next();
+});
 
 const Tour = mongoose.model('Tour', tourSchema);
 
