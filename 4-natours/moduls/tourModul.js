e0fd8938cce77c3c4f2bb97750f3c27f1ec5f0d2
Jami Karimkulov <jami.karimkul@gmail.com>
2024-11-22 14:16:58 -0500

    Add data v alidation: custom validators

--- 4-natours/moduls/tourModul.js
+++ 4-natours/moduls/tourModul.js
@@ -2,6 +2,7 @@
 // const { type } = require('express/lib/response');
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+const validator = require('validator');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -12,6 +13,10 @@ const tourSchema = new mongoose.Schema(
       trim: true,
       maxlength: [40, 'A tour name must have less or equal then 40 characters'],
       minlength: [10, 'A tour name must have less or equal then 10 characters']
+      // validate: [
+      //   validator.isAlpha,
+      //   'Tour name must only contain with characters'
+      // ]
     },
     slug: String,
     duration: {
@@ -45,7 +50,16 @@ const tourSchema = new mongoose.Schema(
       type: Number,
       required: [true, 'A tour must have a price']
     },
-    priceDiscount: Number,
+    priceDiscount: {
+      type: Number,
+      validate: {
+        validator: function(val) {
+          // this only points to current doc on NEW document creation
+          return val < this.price; // 100 < 200 is true
+        },
+        message: 'Discount price ({VALUE}) should be below regular price'
+      }
+    },
     summery: {
       type: String,
       trim: true
