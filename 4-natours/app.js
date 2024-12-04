f2ac68eec854d7a29c4f2259069bc15d5e5a590f
Jami Karimkulov <jami.karimkul@gmail.com>
2024-12-04 13:26:00 -0500

    Add better errors and refactoring

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const app = express();
 const morgan = require('morgan');
+const AppError = require('./utils/appError');
+const globalError = require('./controllers/errorController');
 
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
@@ -24,25 +26,9 @@ app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
 app.all('*', (req, res, next) => {
-  // res.status(400).json({
-  //   status: 'fail',
-  //   message: `Can't find ${req.originalUrl} on this server!`
-  // });
-  const err = new Error(`Can't find ${req.originalUrl} on this server!`);
-  err.status = 'fail';
-  err.statusCode = 404;
-
-  next(err);
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
 
-app.use((err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'error';
-
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message
-  });
-});
+app.use(globalError);
 
 module.exports = app;
