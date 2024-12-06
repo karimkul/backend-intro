cf492fa88b7368039a5ad0dd0766449baadac396
Jami Karimkulov <jami.karimkul@gmail.com>
2024-12-06 13:09:47 -0500

    Errors during development vs production

--- 4-natours/controllers/errorController.js
+++ 4-natours/controllers/errorController.js
@@ -1,9 +1,38 @@
+const sendErrorDev = (res, req) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack
+  });
+};
+
+const sendErrorPro = (err, res) => {
+  // Operational, trusted error: send message to client
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message
+    });
+    // Programming or other unknow error: don't leak error details
+  } else {
+    // 1) Log the error
+    console.error('ERROR ❤️‍🔥', err);
+    //  2)Send genetic message
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went very wrong'
+    });
+  }
+};
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message
-  });
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else if ((process.env.NODE_ENV = 'production')) {
+    sendErrorPro(err, res);
+  }
 };
