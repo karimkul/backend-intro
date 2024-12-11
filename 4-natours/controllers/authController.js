2dc0a64b137b80b6f9e639237296c7ce733be367
Jami Karimkulov <jami.karimkul@gmail.com>
2024-12-11 12:10:57 -0500

    Protect tour routes - part 1

--- 4-natours/controllers/authController.js
+++ 4-natours/controllers/authController.js
@@ -31,21 +31,48 @@ exports.signup = catchAsync(async (req, res, next) => {
 
 exports.login = catchAsync(async (req, res, next) => {
   const { email, password } = req.body;
-  console.log('Email from request body:', email);
+
   // 1) Check if email and password exist
   if (!email || !password) {
     return next(new AppError('Please provid email and password!', 400));
   }
+
   // 2) Check if user exists & password is correct
   const user = await User.findOne({ email }).select('+password');
 
   if (!user || !(await user.correctPassword(password, user.password))) {
     return next(new AppError('Incorrect email or password!', 401));
   }
+
   // 3) Check if everything is ok, Send token to client
   const token = signToken(user._id);
   res.status(200).json({
     status: 'success',
     token
   });
+});
+
+exports.protect = catchAsync(async (req, res, next) => {
+  // 1) Getting token and check of it's there
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    token = req.headers.authorization.split(' ')[1];
+  }
+  console.log(token);
+
+  if (!token) {
+    return next(
+      new AppError('You are not logged in! Please log in to get access.', 401)
+    );
+  }
+  // 2) Verification token
+
+  // 3) Check if user still exists
+
+  // 4) Check if user changed password after the token was issued
+
+  next();
 });
