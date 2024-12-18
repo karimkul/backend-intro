fd383c380e59c9c0de5a94eda507ace0077fffde
Jami Karimkulov <jami.karimkul@gmail.com>
2024-12-17 21:02:38 -0500

    Set security HTTP headers

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 
 const app = express();
 const AppError = require('./utils/appError');
@@ -9,11 +10,17 @@ const globalError = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
-// 1) MIDDLEWARES
+// 1) GLOBAL MIDDLEWARES
+
+// Set security HTTP headers
+app.use(helmet());
+
+// Development logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev')); ///?????
 }
 
+// Limit requests from the same APIs
 const limiter = rateLimit({
   max: 100,
   windowMs: 60 * 60 * 1000,
@@ -21,9 +28,13 @@ const limiter = rateLimit({
 });
 app.use('/api', limiter);
 
-app.use(express.json()); //??????
+// Body parser, reading data into req.body
+app.use(express.json({ limit: '10kb' })); //??????
+
+// Serving static files
 app.use(express.static('./public'));
 
+// Test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   // console.log(req.headers);
