8ba8fc117ccddc10163d469982d89e30325615dd
Jami Karimkulov <jami.karimkul@gmail.com>
2024-12-20 11:03:01 -0500

    Modelling tour guides: child referencing

--- 4-natours/moduls/tourModul.js
+++ 4-natours/moduls/tourModul.js
@@ -2,7 +2,7 @@
 // const { type } = require('express/lib/response');
 const mongoose = require('mongoose');
 const slugify = require('slugify');
-const User = require('./userModul');
+// const User = require('./userModul');
 
 // const validator = require('validator');
 
@@ -109,7 +109,12 @@ const tourSchema = new mongoose.Schema(
         day: Number
       }
     ],
-    guides: Array
+    guides: [
+      {
+        type: mongoose.Schema.ObjectId,
+        ref: 'User'
+      }
+    ]
   },
   {
     toJSON: { virtuals: true },
@@ -127,11 +132,11 @@ tourSchema.pre('save', function(next) {
   next();
 });
 
-tourSchema.pre('save', async function(next) {
-  const guidesPromises = this.guides.map(async id => await User.findById(id));
-  this.guides = await Promise.all(guidesPromises);
-  next();
-});
+// tourSchema.pre('save', async function(next) {
+//   const guidesPromises = this.guides.map(async id => await User.findById(id));
+//   this.guides = await Promise.all(guidesPromises);
+//   next();
+// });
 
 // tourSchema.pre('save', function(next) {
 //   console.log('We will save the documents...');
