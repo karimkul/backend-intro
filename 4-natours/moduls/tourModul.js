1a5e7775d0103b2a5b85758afe0faa57b0637cd5
Jami Karimkulov <jami.karimkul@gmail.com>
2024-12-20 11:24:56 -0500

    Populate tour guides

--- 4-natours/moduls/tourModul.js
+++ 4-natours/moduls/tourModul.js
@@ -156,6 +156,14 @@ tourSchema.pre(/^find/, function(next) {
   next();
 });
 
+tourSchema.pre(/^find/, function(next) {
+  this.populate({
+    path: 'guides',
+    select: '-__v -passwordChangedAt'
+  });
+  next();
+});
+
 tourSchema.post(/^find/, function(docs, next) {
   // this.find({ secretTour: { $ne: true } });
   console.log(`Query took ${Date.now() - this.start} milliseconds`);
