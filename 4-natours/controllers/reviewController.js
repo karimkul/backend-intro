50a2dad6be33f0ca5beef251a2f124de8b33ac3d
Jami Karimkulov <jami.karimkul@gmail.com>
2024-12-22 12:39:38 -0500

    Factory functions: reading

--- 4-natours/controllers/reviewController.js
+++ 4-natours/controllers/reviewController.js
@@ -1,21 +1,5 @@
 const Review = require('../moduls/reviewModul');
-const catchAsync = require('../utils/catchAsync');
 const factory = require('./handlerFactory');
-
-exports.getAllReview = catchAsync(async (req, res, next) => {
-  let filter = {};
-  if (req.params.tourId) filter = { tour: req.params.tourId };
-
-  const reviews = await Review.find(filter);
-
-  res.status(200).json({
-    status: 'success',
-    results: reviews.length,
-    data: {
-      reviews
-    }
-  });
-});
 
 exports.setTourUserIds = (req, res, next) => {
   // Allow nested routes
@@ -24,7 +8,8 @@ exports.setTourUserIds = (req, res, next) => {
 
   next();
 };
-
+exports.getAllReview = factory.getAll(Review);
+exports.getReview = factory.getOne(Review);
 exports.createReview = factory.createOne(Review);
 exports.updateReview = factory.updateOne(Review);
 exports.deleteReview = factory.deleteOne(Review);
