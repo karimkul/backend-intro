ee0e59ef6b0737f68bafd8e3d142064c2c36cb22
Jami Karimkulov <jami.karimkul@gmail.com>
2024-12-22 10:13:14 -0500

    Build handler factory functions: delete

--- 4-natours/controllers/reviewController.js
+++ 4-natours/controllers/reviewController.js
@@ -1,9 +1,11 @@
 const Review = require('../moduls/reviewModul');
 const catchAsync = require('../utils/catchAsync');
+const factory = require('./handlerFactory');
 
 exports.getAllReview = catchAsync(async (req, res, next) => {
   let filter = {};
   if (req.params.tourId) filter = { tour: req.params.tourId };
+
   const reviews = await Review.find(filter);
 
   res.status(200).json({
@@ -29,3 +31,5 @@ exports.createReview = catchAsync(async (req, res, next) => {
     }
   });
 });
+
+exports.deleteReview = factory.deleteOne(Review);
