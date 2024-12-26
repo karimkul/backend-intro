06956ca2a0de9d167159b31361adbd243ade5510
Jami Karimkulov <jami.karimkul@gmail.com>
2024-12-26 17:55:57 -0500

    Include a map with mapbox - part 1

--- 4-natours/controllers/viewsController.js
+++ 4-natours/controllers/viewsController.js
@@ -22,7 +22,7 @@ exports.getTour = catchAsync(async (req, res) => {
   });
 
   res.status(200).render('tour', {
-    title: 'The Forest Hiker',
+    title: `${tour.name} Tour`,
     tour
   });
 });
