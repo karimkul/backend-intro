f39bae7e3d2ae9500c4f856a9c2a7a57af895dd3
Jami Karimkulov <jami.karimkul@gmail.com>
2024-12-26 09:20:19 -0500

    Build the tour overview - part 1

--- 4-natours/controllers/viewsController.js
+++ 4-natours/controllers/viewsController.js
@@ -1,9 +1,18 @@
-exports.getOverview = (req, res) => {
-  res.status(200).render('base', {
-    tour: 'The Forest Hiker',
-    user: 'Jami'
+const Tour = require('../moduls/tourModul');
+const catchAsync = require('../utils/catchAsync');
+
+exports.getOverview = catchAsync(async (req, res, next) => {
+  // 1) Get tour data from collection
+  const tours = await Tour.find();
+
+  // 2) Build template
+
+  // 3) Render that template using tour data from step 1)
+  res.status(200).render('overview', {
+    title: 'All tours',
+    tours
   });
-};
+});
 
 exports.getTour = (req, res) => {
   res.status(200).render('tour', {
