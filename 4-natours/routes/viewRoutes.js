136a504f1099583af4d8cc412fcd5dd69ace3f51
Jami Karimkulov <jami.karimkul@gmail.com>
2024-12-30 11:25:41 -0500

    Logging in users with our API - part 2

--- 4-natours/routes/viewRoutes.js
+++ 4-natours/routes/viewRoutes.js
@@ -4,9 +4,10 @@ const authController = require('../controllers/authController');
 
 const router = express.Router();
 
-router.get('/', viewController.getOverview);
-router.get('/tour/:slug', authController.protect, viewController.getTour);
+router.use(authController.isLoggedin);
 
+router.get('/', viewController.getOverview);
+router.get('/tour/:slug', viewController.getTour);
 router.get('/login', viewController.getLoginForm);
 
 module.exports = router;
